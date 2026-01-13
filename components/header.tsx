cec6f1289b23b92bafef35284822a37d11c077fc
robertmain53 <mainrobert53@gmail.com>
2026-01-13 15:23:22 +0100

    f

--- components/header.tsx
+++ components/header.tsx
@@ -28,6 +28,12 @@ export function Header() {
           <Link href="/prayer-points" className="hover:text-primary transition-colors">
             Prayer Points
           </Link>
+             <Link href="/bible-places" className="hover:text-primary transition-colors">
+            Bible Places
+          </Link>
+            <Link href="/bible-travel" className="hover:text-primary transition-colors">
+            Bible Travel
+          </Link>
           <Link href="/about" className="hover:text-primary transition-colors">
             About
           </Link>
